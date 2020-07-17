9da9a4f759e20662ed67ade2433d0b224ea730b8
ThomasStuart <thomas.j.stuart96@gmail.com>
2020-07-17 12:01:16 -0700

    customize the row preview

--- Landmarks/Build/Intermediates/Previews/Landmarks/Intermediates.noindex/BuildingListsAndNavigation.build/Debug-iphonesimulator/Landmarks.build/Objects-normal/x86_64/LandmarkRow.3.preview-thunk.swift
+++ Landmarks/Build/Intermediates/Previews/Landmarks/Intermediates.noindex/BuildingListsAndNavigation.build/Debug-iphonesimulator/Landmarks.build/Objects-normal/x86_64/LandmarkRow.3.preview-thunk.swift
@@ -4,8 +4,13 @@ import SwiftUI
 
 extension LandmarkRow_Previews {
     @_dynamicReplacement(for: previews) private static var __preview__previews: some View {
-        #sourceLocation(file: "/Users/Thomas_Stuart/Desktop/XcodeProjects/BuildingListsAndNavigation/Landmarks/Landmarks/Models/LandmarkRow.swift", line: 28)
-        AnyView(__designTimeSelection(LandmarkRow(landmark: __designTimeSelection(landmarkData[__designTimeInteger("#6363.[2].[0].property.[0].[0].arg[0].value.[0].value", fallback: 0)], "#6363.[2].[0].property.[0].[0].arg[0].value")), "#6363.[2].[0].property.[0].[0]"))
+        #sourceLocation(file: "/Users/Thomas_Stuart/Desktop/XcodeProjects/BuildingListsAndNavigation/Landmarks/Landmarks/Models/LandmarkRow.swift", line: 29)
+    
+        AnyView(__designTimeSelection(Group {
+        __designTimeSelection(LandmarkRow(landmark: __designTimeSelection(landmarkData[__designTimeInteger("#6363.[2].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[0].value", fallback: 0)], "#6363.[2].[0].property.[0].[0].arg[0].value.[0].arg[0].value")), "#6363.[2].[0].property.[0].[0].arg[0].value.[0]")
+        __designTimeSelection(LandmarkRow(landmark: __designTimeSelection(landmarkData[__designTimeInteger("#6363.[2].[0].property.[0].[0].arg[0].value.[1].arg[0].value.[0].value", fallback: 1)], "#6363.[2].[0].property.[0].[0].arg[0].value.[1].arg[0].value")), "#6363.[2].[0].property.[0].[0].arg[0].value.[1]")
+      }
+        .previewLayout(.fixed(width:__designTimeInteger("#6363.[2].[0].property.[0].[0].modifier[0].arg[0].value.arg[0].value", fallback: 300), height:__designTimeInteger("#6363.[2].[0].property.[0].[0].modifier[0].arg[0].value.arg[1].value", fallback: 70))), "#6363.[2].[0].property.[0].[0]"))
 #sourceLocation()
     }
 }
